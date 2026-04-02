15f353f23e0f4300146547e2f0d0baffefb23276
Peter Chiang <pccchiang@gmail.com>
2026-04-02 13:57:54 -0400

    Add updated minimax solution

--- mastermind.py
+++ mastermind.py
@@ -33,45 +33,52 @@ def check_two(guess: Iterable[str], combo: Iterable[str]):
 
 
 m = Mastermind()
+print("Answer", m.answer)
 
 combos = set(product("123456", repeat=4))
 
 guess = ["1", "1", "2", "2"]
-turn = 1
-output = m.check_guess("".join(guess))
-while output[0] != 4:
+turn = 0
+while True:
+    turn += 1
+    output = m.check_guess("".join(guess))
+    if output[0] == 4:
+        break
     print("Turn", turn, "Guess", guess, "Output", output)
+
     pruned_combos = set()
     guess_count = Counter(guess)
     for combo in combos:
-        matches = check_two(guess, combo)
-        if matches[0] >= output[0] and matches[1] >= output[1]:
+        if output == check_two(guess, combo):
             pruned_combos.add(combo)
-
-    print("Pruned Combos count", len(combos) - len(pruned_combos))
-    guess = pruned_combos.pop()
-    output = m.check_guess("".join(guess))
+    print("Pruned", len(combos) - len(pruned_combos))
     combos = pruned_combos
-    
-    guess = pruned_combos.pop()
+
     worst_response_scores = Counter()
     for combo in product("123456", repeat=4):
-        combo = ''.join(combo)
-        for black in range(5):
-            for white in range(5):
-                if black + white > 4:
-                    continue
-                current_response = 0
-                for possible_combo in pruned_combos:
-                    response = check_two(combo, possible_combo)
-                    if response[0] >= black and response[1] >= white:
-                        current_response += 1
-                        worst_response_scores[combo] = max(worst_response_scores[combo], current_response)
+        combo_responses = Counter()
+        for pruned_combo in pruned_combos:
+            response = check_two(combo, pruned_combo)
+            combo_responses[response] += 1
+        worst_response_scores[combo] = max(combo_responses.values())
+
     min_response_score = min(worst_response_scores.values())
-    possible_guesses = [guess for guess in worst_response_scores.keys() if worst_response_scores[guess] == min_response_score]
+    possible_guesses = [
+        guess
+        for guess in worst_response_scores.keys()
+        if worst_response_scores[guess] == min_response_score
+    ]
+    possible_guesses.sort()
+
+    next_guess = None
     for possible_guess in possible_guesses:
         if possible_guess in combos:
-            guess = possible_guess
-            continue
-    guess = possible_guesses.pop()
+            next_guess = possible_guess
+            break
+
+    if not next_guess:
+        next_guess = possible_guesses[0]
+
+    guess = next_guess
+
 print("Won in turn", turn, "Answer", guess)
