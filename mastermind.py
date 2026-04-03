d01dc7b93b3f131f999b2a9649aa8dccbc0fd602
Peter Chiang <pccchiang@gmail.com>
2026-04-03 13:29:47 -0400

    refactor to add trying to guess and possibilities

--- mastermind.py
+++ mastermind.py
@@ -32,53 +32,70 @@ def check_two(guess: Iterable[str], combo: Iterable[str]):
     return (black, white)
 
 
-m = Mastermind()
-print("Answer", m.answer)
+def minimax_guess(m: Mastermind) -> int:
+    print("Answer", m.answer)
 
-combos = set(product("123456", repeat=4))
+    combos = set(product("123456", repeat=4))
 
-guess = ["1", "1", "2", "2"]
-turn = 0
-while True:
-    turn += 1
-    output = m.check_guess("".join(guess))
-    if output[0] == 4:
-        break
-    print("Turn", turn, "Guess", guess, "Output", output)
-
-    pruned_combos = set()
-    guess_count = Counter(guess)
-    for combo in combos:
-        if output == check_two(guess, combo):
-            pruned_combos.add(combo)
-    print("Pruned", len(combos) - len(pruned_combos))
-    combos = pruned_combos
-
-    worst_response_scores = Counter()
-    for combo in product("123456", repeat=4):
-        combo_responses = Counter()
-        for pruned_combo in pruned_combos:
-            response = check_two(combo, pruned_combo)
-            combo_responses[response] += 1
-        worst_response_scores[combo] = max(combo_responses.values())
-
-    min_response_score = min(worst_response_scores.values())
-    possible_guesses = [
-        guess
-        for guess in worst_response_scores.keys()
-        if worst_response_scores[guess] == min_response_score
-    ]
-    possible_guesses.sort()
-
-    next_guess = None
-    for possible_guess in possible_guesses:
-        if possible_guess in combos:
-            next_guess = possible_guess
+    guess = ["1", "1", "2", "2"]
+    turn = 0
+    while True:
+        turn += 1
+        output = m.check_guess("".join(guess))
+        if output[0] == 4:
             break
+        print("Turn", turn, "Guess", guess, "Output", output)
 
-    if not next_guess:
-        next_guess = possible_guesses[0]
+        pruned_combos = set()
+        for combo in combos:
+            if output == check_two(guess, combo):
+                pruned_combos.add(combo)
+        print(
+            "Pruned", len(combos) - len(pruned_combos), "Remaining", len(pruned_combos)
+        )
+        combos = pruned_combos
 
-    guess = next_guess
+        worst_response_scores = Counter()
+        for combo in product("123456", repeat=4):
+            combo_responses = Counter()
+            for pruned_combo in pruned_combos:
+                response = check_two(combo, pruned_combo)
+                combo_responses[response] += 1
+            worst_response_scores[combo] = max(combo_responses.values())
 
-print("Won in turn", turn, "Answer", guess)
+        min_response_score = min(worst_response_scores.values())
+        possible_guesses = [
+            guess
+            for guess in worst_response_scores.keys()
+            if worst_response_scores[guess] == min_response_score
+        ]
+        possible_guesses.sort()
+
+        next_guess = None
+        for possible_guess in possible_guesses:
+            if possible_guess in combos:
+                next_guess = possible_guess
+                break
+
+        if not next_guess:
+            next_guess = possible_guesses[0]
+
+        guess = next_guess
+
+    print("Won in turn", turn, "Answer", guess)
+    return turn
+
+
+def try_all():
+    counts = Counter()
+    for combo in product("123456", repeat=4):
+        m = Mastermind()
+        m.answer = combo
+        counts[minimax_guess(m)] += 1
+
+    print(counts)
+
+
+if __name__ == "__main__":
+    m = Mastermind()
+    minimax_guess(m)
