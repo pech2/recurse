6887d58c1627ef601bd5c4dfc035da0b41cdeb84
Peter Chiang <pccchiang@gmail.com>
2026-04-03 17:37:13 -0400

    Add explanation for minimax algo

--- mastermind.py
+++ mastermind.py
@@ -33,10 +33,21 @@ def check_two(guess: Iterable[str], combo: Iterable[str]):
 
 
 def minimax_guess(m: Mastermind) -> int:
+    """Guess the answer in Mastermind using Knuth's Minimax algorithm
+
+    1. Start with guess 1122 which has the best outcome and get a certain response (a combination of black and white pegs).
+    2. For each guess we keep track of remaining possible guesses and prune what's possible after each guess.
+        a. Prune by keeping only possible guesses that match each guess response.
+    3. Calculate next guess by choosing the least worst option.
+        a. Score guesses by the least guesses that would be removed from making that guess by simulating all responses for all guesses.
+        b. Choose the best of the guesses.
+    """
     print("Answer", m.answer)
 
+    # Create a set with all possible guesses
     combos = set(product("123456", repeat=4))
 
+    # Start with 1122 because it has an AABB pattern which has the best outcome rather than ABCD
     guess = ["1", "1", "2", "2"]
     turn = 0
     while True:
@@ -46,6 +57,11 @@ def minimax_guess(m: Mastermind) -> int:
             break
         print("Turn", turn, "Guess", guess, "Output", output)
 
+        # Which check every remaining possible guess to exactly match the response against the guess we made.
+        #
+        # We make an exact match because say we have a potential guess 1123. It would match 1122 with 3 black black pegs.
+        # If our response for guess 1122 was 2 black pegs we can remove potential guess 1123 even though it has at least the same response with 1 more black peg.
+        # Consider if we guessed 1123 instead. Since 1123 and 1122 match in the first 3 positions, we know that the
         pruned_combos = set()
         for combo in combos:
             if output == check_two(guess, combo):
