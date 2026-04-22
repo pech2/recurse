21c891056b3f716d6978a74b8920ce7bf15826ae
Peter Chiang <pccchiang@gmail.com>
2026-04-22 15:30:12 -0400

    solve

--- Rust/rustlings/exercises/quizzes/quiz2.rs
+++ Rust/rustlings/exercises/quizzes/quiz2.rs
@@ -28,6 +28,18 @@ mod my_module {
 
     // TODO: Complete the function as described above.
     // pub fn transformer(input: ???) -> ??? { ??? }
+    pub fn transformer(input: Vec<(String, Command)>) -> Vec<String> {
+        let mut transformed = Vec::new();
+
+        for (string, command) in input {
+            match command {
+                Command::Uppercase => transformed.push(string.to_uppercase()),
+                Command::Trim => transformed.push(string.trim().to_string()),
+                Command::Append(count) => transformed.push(string + &"bar".repeat(count)),
+            }
+        }
+        transformed
+    }
 }
 
 fn main() {
@@ -38,6 +50,7 @@ fn main() {
 mod tests {
     // TODO: What do we need to import to have `transformer` in scope?
     // use ???;
+    use super::my_module::transformer;
     use super::Command;
 
     #[test]
