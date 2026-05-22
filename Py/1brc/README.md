2eca0daa73b7be83b7ffb205924970ed380c6b01
Peter Chiang <pccchiang@gmail.com>
2026-05-21 23:47:58 -0400

    unoptimized solution

---
+++ Py/1brc/README.md
@@ -0,0 +1,9 @@
+## Findings
+
+### 1 Million (13.8 MB)
+
+- Unoptimized
+  - .27 seconds
+  - Using defaultdict with lambdas
+  - Printing out results for each station with its min, avg, and max
+  - Just added printing to only 2 decimals places which seems .01 seconds faster
--- Py/1brc/main.py
+++ Py/1brc/main.py
@@ -1,20 +1,29 @@
 from collections import defaultdict
+from time import perf_counter
 
 
 def main():
-    mins = {}
-    maxs = {}
+    mins = defaultdict(lambda: float("inf"))
+    maxs = defaultdict(lambda: float("-inf"))
     avgs = defaultdict(list)
 
     # Bamako;38.8
     with open("measurements-1M.txt") as f:
         for line in f:
-            station, temp, _ = line.split(";")
-            temp = int(temp)
+            station, temp = line.strip().split(";")
+            temp = float(temp)
             mins[station] = min(mins[station], temp)
             maxs[station] = max(maxs[station], temp)
             avgs[station].append(temp)
+    for station in mins:
+        print(
+            f"{station}: {mins[station]}, {sum(avgs[station]) / len(avgs[station]):,.2f}, {maxs[station]}"
+        )
 
 
 if __name__ == "__main__":
+    start = perf_counter()
     main()
+    end = perf_counter()
+
+    print(f"Time elapsed: {round(end - start, 2)} seconds")
